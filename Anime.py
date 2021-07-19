01add4e2615d7bf0ed2ee1ac2ec0fb601601018e
Ryan <facebook0209@gmail.com>
2021-07-20 00:02:27 +0800

    follow main branch update

--- Anime.py
+++ Anime.py
@@ -143,20 +143,26 @@ class Anime():
         self._bangumi_name = re.sub(r'\s+', ' ', self._bangumi_name)  # 去除重复空格
 
     def __get_episode(self):  # 提取集数
+
+        def get_ep():
+            # 20210719 动画疯的版本位置又瞎蹦跶
+            # https://github.com/miyouzi/aniGamerPlus/issues/109
+            # 先查看有沒有數字, 如果沒有再查看有沒有中括號, 如果都沒有直接放棄, 把集數填作 1
+            self._episode = re.findall(r'\[\d*\.?\d* *\.?[A-Z,a-z]*(?:電影)?\]', self._title)
+            if len(self._episode) > 0:
+                self._episode = str(self._episode[0][1:-1])
+            elif len(re.findall(r'\[.+?\]', self._title)) > 0:
+                self._episode = re.findall(r'\[.+?\]', self._title)
+                self._episode = str(self._episode[0][1:-1])
+            else:
+                self._episode = "1"
+
         # 20200320 发现多版本标签后置导致原集数提取方法失效
         # https://github.com/miyouzi/aniGamerPlus/issues/36
         # self._episode = re.findall(r'\[.+?\]', self._title)  # 非贪婪匹配
         # self._episode = str(self._episode[-1][1:-1])  # 考虑到 .5 集和 sp、ova 等存在，以str储存
         if self._settings['use_mobile_api']:
-            self._episode = re.findall(r'\[\d*\.?\d* *\.?[A-Z,a-z]*\]', self._title)
-            if len(self._episode) > 0:
-              self._episode = str(self._episode[0][1:-1])
-            elif (len(re.findall(r'\[.+?\]', self._title)) > 0):
-              self._episode = re.findall(r'\[.+?\]', self._title)
-              self._episode = str(self._episode[0][1:-1])
-            else:
-              self._episode = "1"
-
+            get_ep()
         else:
             soup = self._src
             try:
@@ -165,14 +171,9 @@ class Anime():
             except AttributeError:
                 # 如果这个sn就一集, 不存在剧集列表的情况
                 # https://github.com/miyouzi/aniGamerPlus/issues/36#issuecomment-605065988
-                self._episode = re.findall(r'\[\d*\.?\d* *\.?[A-Z,a-z]*\]', self._title)
-                if len(self._episode) > 0:
-                  self._episode = str(self._episode[0][1:-1])
-                elif (len(re.findall(r'\[.+?\]', self._title)) > 0):
-                  self._episode = re.findall(r'\[.+?\]', self._title)
-                  self._episode = str(self._episode[0][1:-1])
-                else:
-                  self._episode = "1"
+                # self._episode = re.findall(r'\[.+?\]', self._title)  # 非贪婪匹配
+                # self._episode = str(self._episode[0][1:-1])  # 考虑到 .5 集和 sp、ova 等存在，以str储存
+                get_ep()
 
     def __get_episode_list(self):
         if self._settings['use_mobile_api']:
